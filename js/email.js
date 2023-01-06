882024a0a9a011c44ca0ac1f760f029b0b53c909
Dev-scott <sadoscott25@gmail.com>
2023-01-06 06:38:20 +0100

    mise a jour

--- js/email.js
+++ js/email.js
@@ -17,7 +17,8 @@ function SendMail() {
         to_name: 'Dev scott',
         from_name:document.getElementById('fullName').value,
         message: document.getElementById('message').value,
-
+        user_name:document.getElementById('fullName').value,
+        user_email:document.getElementById('email').value,
     }
     emailjs.send("service_c64e4wx","template_rekwibr",params).then(function(res){
 
